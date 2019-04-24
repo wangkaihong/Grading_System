7f713aa4c4f6094308f52480a6cbba8e95c4b5cd
zhx9225@gmail.com <Helena05!>
2019-04-24 17:23:31 -0400

    change score

--- src/FrontEnd/GradeSheet_UI.java
+++ src/FrontEnd/GradeSheet_UI.java
@@ -119,11 +119,11 @@ public class GradeSheet_UI extends JFrame implements ActionListener{
         mSheet.addTableModelListener(new TableModelListener() {
             @Override
             public void tableChanged(TableModelEvent e) {
-                if(e.getColumn() >  columnNames.length && e.getFirstRow() != 0 ){
+                if(e.getColumn() <  columnNames.length && e.getFirstRow() != 0 ){
                     int row = e.getFirstRow();
                     int col = e.getColumn();
                     //Object value = mSheet.getValueAt(row,col);
-                    double value = (double)  mSheet.getValueAt(row,col);
+                    double value = Double.parseDouble((String)mSheet.getValueAt(row,col));
                     //save changed score to sheet
                     course.getSheet().setScore(row, col,value);
                     System.out.println(value +" ----change or add score");
@@ -187,7 +187,7 @@ public class GradeSheet_UI extends JFrame implements ActionListener{
         tSheet.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                noteText.setText("");
+                noteText.setText(" ");
                 int row = tSheet.getSelectedRow();
                 int col = tSheet.getSelectedColumn();
                 String pullNote = course.getNote(row, col).toString();
--- src/FrontEnd/ModifyCol_UI.java
+++ src/FrontEnd/ModifyCol_UI.java
@@ -84,13 +84,6 @@ public class ModifyCol_UI extends JFrame implements ActionListener {
         this.setSize(1000, 600);
         this.setTitle("Modify Column");
         this.setVisible(true);
-
-
-
-
-    }
-    public void actionPerformed(ActionEvent e) {
-
         tSheet.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -112,6 +105,11 @@ public class ModifyCol_UI extends JFrame implements ActionListener {
                 }
             }
         });
+
+    }
+    public void actionPerformed(ActionEvent e) {
+
+
         if (e.getSource() == back) {
             dispose();
             new GradeSheet_UI(grading_system,course);
